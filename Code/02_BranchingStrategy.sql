3cc5d45278e5b36d61fa217ffe63cd33cc7b10dc
Steve Jones <way0utwest@hotmail.com>
2015-08-13 17:10:25 -0600

    Added notes to slides for presenter. Updated formatting for demo slides

--- Code/02_BranchingStrategy.sql
+++ Code/02_BranchingStrategy.sql
@@ -1,10 +1,21 @@
 /*
 Branching and Merging Database Code
 
-
+Simple Table Merge
 
 Copyright 2014 Steve Jones
 
 This code is provided as is with no warranty implied.
 
 */
+-- create table in trunk
+CREATE TABLE Users(username VARCHAR(100));
+-- commit
+
+-- branch to status feature
+
+ALTER TABLE users ADD status INT NOT NULL DEFAULT 1;
+
+-- commit
+
+-- merge from file system.
